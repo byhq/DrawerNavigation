298edbab36980ce7b6709d926105b0d892c862cd
byhq <baihaqialfan3@gmail.com>
2023-10-11 22:16:54 +0700

    Telegram Navigation

---
+++ app/src/main/java/com/example/drawernav/PanggilanFragment.java
@@ -0,0 +1,19 @@
+package com.example.drawernav;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PanggilanFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_panggilan, container, false);
+    }
+}
